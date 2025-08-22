98041f57cccc114b1f04e735f66ff08222caf6ec
hemanth.p <hemanth.p@flatworldsolutions.com>
2025-08-22 10:33:53 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-import uvicorn
-from scripts.config import ModuleConfig
+# import uvicorn
+# from scripts.config import ModuleConfig
+from mangum import Mangum
+from main import app as fastapi_app
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host=ModuleConfig.HOST,
-        port=ModuleConfig.PORT,
-        reload=ModuleConfig.RELOAD_ASGI,
-    )
+# if __name__ == "__main__":
+#     # uvicorn.run(
+#     #     "main:app",
+#     #     host=ModuleConfig.HOST,
+#     #     port=ModuleConfig.PORT,
+#     #     reload=ModuleConfig.RELOAD_ASGI,
+#     # )
+handler = Mangum(fastapi_app)
